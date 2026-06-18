a069dca65fafc06844149736d6325229668c26eb
GustavoRodak-Pormade <gustavo.rodak@pormade.com.br>
2026-06-18 17:35:01 -0300

    primeiros começos

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, View } from "react-native";
+
+export default function Navbar(){
+  return(
+    <View>
+
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    
+  }
+})
---
+++ pages/cadastrarMulta/cadastrarmulta.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+import Navbar from "../../components/navbar/Navbar";
+
+export default function CadastrarMulta() {
+  return (
+    <View>
+      <Navbar />
+      <View>
+        <Text>teste</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
