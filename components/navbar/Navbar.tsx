6f367d1d883cf6f72be8696edf42ec0f129a704a
GustavoRodak-Pormade <gustavo.rodak@pormade.com.br>
2026-06-18 17:50:59 -0300

    primeiros estilos

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, View } from "react-native";
 
 export default function Navbar(){
   return(
-    <View>
+    <View style={style.container}>
 
     </View>
   )
@@ -10,8 +10,13 @@ export default function Navbar(){
 
 const style = StyleSheet.create({
   container: {
-    display: "flex",
-    flexDirection: "row",
-    
-  }
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginTop: 35,
+        padding: 13,
+        borderBottomWidth: 2,
+        borderBottomColor: "#e7e7e7ff"
+    },
 })
